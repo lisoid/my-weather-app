8c0f6293570cdf2cc4766407440c6ebfda1deec9
lisoid <lisoid@ukr.net>
2024-02-14 11:20:55 +0200

    Add show forecast parameters + error handling
    + slight CSS improvements

--- scripts/index.js
+++ scripts/index.js
@@ -47,28 +47,37 @@ document.addEventListener("DOMContentLoaded", (event) => {
     getForecastAPI(response.data.city);
   }
 
+  //format days (short version) for weather forecast
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[date.getDay()];
+  }
+
   //Show weather forecast
   function showForecast(response) {
-    console.log(response.data);
-
-    let days = ["Thu", "Fri", "Sat", "Sun", "Mon"];
     let forecastHTML = "";
 
-    days.forEach(function (day) {
+    response.data.daily.slice(1, 6).forEach(function (day) {
       forecastHTML =
         forecastHTML +
         `
 <div class="weather-forecast-day">
-  <div class="weather-forecast-date">Thu</div>
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
   <div class="weather-forecast-icon">
     <img
-      src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/clear-sky-day.png"
+      src="${day.condition.icon_url}"
       alt=""
     />
   </div>
   <div class="weather-forecast-temperatures">
-    <span class="weather-forecast-temp-max">18°</span>
-    <span class="weather-forecast-temp-min">12°</span>
+    <span class="weather-forecast-temp-max"'>${Math.round(
+      day.temperature.maximum
+    )}°</span>
+    <span class="weather-forecast-temp-min">${Math.round(
+      day.temperature.minimum
+    )}°</span>
   </div>
 </div>`;
     });
@@ -111,9 +120,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
       })
       .catch((error) => {
         console.error("Error fetching weather data:", error);
-        alert(
-          `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-        );
+        alert(`Please try again`);
       });
   }
 
@@ -121,8 +128,22 @@ document.addEventListener("DOMContentLoaded", (event) => {
   function getForecastAPI(city) {
     let apiKey = "0daa30c42c0776b6ft149co23bec4055";
     let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&unit=metric`;
-    console.log(apiUrl);
-    axios.get(apiUrl).then(showForecast);
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        if (response.data && response.data.city) {
+          showForecast(response);
+        } else {
+          console.error("Error: Forecast not found in the response data.");
+          alert(
+            `Error: The forecast for specified city "${city}" was not found.`
+          );
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching weather data:", error);
+        alert(`Please try again`);
+      });
   }
 
   let newCity = document.querySelector("#search-form");
